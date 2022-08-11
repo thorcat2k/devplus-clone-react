44027d91a50b486855b51338f4cabd65afa4d338
Tu Nguyen <tu.nguyen@devplus.edu.vn>
2022-08-11 16:27:38 +0700

    add review component to homepage

--- src/Screen/HomePage/HomePage.js
+++ src/Screen/HomePage/HomePage.js
@@ -3,14 +3,16 @@ import Campus from "../../Components/Campus/Campus";
 import Common from "../../Components/Common/Common";
 import Footer from "../../Components/Footer/Footer";
 import Receive from "../../Components/Receive/Receive";
+import Review from "../../Components/Review/Review";
 
 function HomePage() {
   return (
     <>
       <AboutDev />
-      <Receive/>
+      <Receive />
       <Campus />
       <Common />
+      <Review />
       <Footer />
     </>
   );
