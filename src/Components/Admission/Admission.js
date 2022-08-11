72663d990bf63919509f677d6759eca2f2cdd886
dathuynh021100 <dat.huynh@devplus.edu.vn>
2022-08-11 16:59:55 +0700

    create admission component to homepage

---
+++ src/Components/Admission/Admission.js
@@ -0,0 +1,33 @@
+import "./Admission.css";
+const Admission = () => {
+  return (
+    <section className="admission">
+      <div className="admission-container">
+        <div className="img-admission">
+          <img
+            src="https://devplus.asia/assets/images/devplus/Admission_for_2021.png"
+            alt=""
+          />
+        </div>
+        <div className="content-admission fade-up">
+          <p id="title-admission">Admission for 2021</p>
+          <p id="text-admission">
+            Disclaimer: This position is expected to start around Feb 2022 and
+            continue through the entire Summer term. We ask for a minimum of 12
+            weeks, full-time, for most internships. Please consider before
+            submitting an application.
+            <br />
+            Devplus aims to provide students the chance to work with our clients
+            and awesome mentors to level up your programing skillset in the
+            RIGHT path. With your education and experience, you will be able to
+            take on real-world challenges from day one.
+          </p>
+          <button className="button" href="">
+            <span id="button-admission">APPLY NOW</span>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Admission;
