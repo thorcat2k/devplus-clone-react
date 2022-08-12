4648d6fdd67264f6a98fa354a56ec435f35fbb2f
ngoc_ng <nmtngoc238@gmail.com>
2022-08-12 09:42:40 +0700

    add header v2

--- src/Screen/HomePage/HomePage.js
+++ src/Screen/HomePage/HomePage.js
@@ -6,10 +6,12 @@ import Common from "../../Components/Common/Common";
 import Footer from "../../Components/Footer/Footer";
 import Receive from "../../Components/Receive/Receive";
 import Review from "../../Components/Review/Review";
+import Navbar from "../../Components/Header/Navbar/Navbar"
 
 function HomePage() {
   return (
     <>
+      <Navbar/>
       <Banner />
       <AboutDev />
       <Receive />
